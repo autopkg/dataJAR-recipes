1a1e2710f4607a0bebd2d273f9a93e362f894c92
macmule <ben@macmule.com>
2021-07-24 23:10:38 +0100

    Update BrowserStackLocalBinaryVersioner.py

--- BrowserStackLocal/BrowserStackLocalBinaryVersioner.py
+++ BrowserStackLocal/BrowserStackLocalBinaryVersioner.py
@@ -83,7 +83,7 @@ class BrowserStackLocalBinaryVersioner(Processor):
             # Get binary version, from: https://www.browserstack.com/local-testing/binary-params
             # raise if we error
             try:
-                version = subprocess.check_output([binary_path, '--version']
+                version = subprocess.check_output([self.env['binary_path'], '--version']
                     ).split()[3].decode('utf-8')
             except subprocess.CalledProcessError:
                 raise ProcessorError("Encountered an error when trying to get the "
